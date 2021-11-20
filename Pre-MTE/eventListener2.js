801d2d90394d747d032d88ae663de61478650e5e
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-11-20 13:29:19 +0530

    Add files

--- Pre-MTE/eventListener2.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("submit").addEventListener('click', function(event){
-    // NOTE: You are clicking a submit button.  After this function runs,
-    // then the form will be submitted.  If you want to *stop* that, you can
-    // use the following:
-    // event.preventDefault();
-
-    // In here `this` will be the element that was clicked, the submit button
-     document.getElementById('mytext').innerHTML += "Hello Hi";
-});
